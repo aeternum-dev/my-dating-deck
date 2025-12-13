79c72d4dc172efc9308f90f6d902318ff4e39085
aeternum-dev <viccesvok4@gmail.com>
2025-12-13 20:47:59 +0100

    Make thobbies columns responsive

--- src/components/slides/HobbiesSlide.tsx
+++ src/components/slides/HobbiesSlide.tsx
@@ -26,7 +26,7 @@ const HobbiesSlide = () => {
           </h2>
         </div>
 
-        <div className="grid grid-cols-2 gap-3">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
           {hobbies.map((hobby, index) => (
             <motion.div
               key={hobby.name}
